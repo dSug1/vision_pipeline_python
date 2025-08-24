08a411c06d20dd5a5c870af43a185127eec2882c
dSug1 <98550633+dSug1@users.noreply.github.com>
2025-08-24 18:02:40 +0300

    Resized the windows to fullscreen using the NuGet package in description below
    Microsoft.WindowsAppSDK

--- MauiApp_Launcher/MauiProgram.cs
+++ MauiApp_Launcher/MauiProgram.cs
@@ -1,4 +1,9 @@
 ﻿using Microsoft.Extensions.Logging;
+using Microsoft.Maui.LifecycleEvents;
+#if WINDOWS
+using Microsoft.UI;
+using Microsoft.UI.Windowing;
+#endif
 
 namespace MauiApp_Launcher
 {
@@ -15,8 +20,26 @@ namespace MauiApp_Launcher
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
                 });
 
+            // Add custom lifecycle configuration here so that the display occupies the full screen
+            builder.ConfigureLifecycleEvents(events =>
+            {
+#if WINDOWS
+                events.AddWindows(w =>
+                {
+                    w.OnWindowCreated(window =>
+                    {
+                        window.ExtendsContentIntoTitleBar = true;
+                        IntPtr hWnd = WinRT.Interop.WindowNative.GetWindowHandle(window);
+                        WindowId myWndId = Win32Interop.GetWindowIdFromWindow(hWnd);
+                        var _appWindow = AppWindow.GetFromWindowId(myWndId);
+                        _appWindow.SetPresenter(AppWindowPresenterKind.FullScreen);
+                    });
+                });
+#endif
+            });
+
 #if DEBUG
-    		builder.Logging.AddDebug();
+            builder.Logging.AddDebug();
 #endif
 
             return builder.Build();
