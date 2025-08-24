1e332f12e2343a2c81da87f3e4d6f5f196056b3e
dSug1 <98550633+dSug1@users.noreply.github.com>
2025-08-24 19:40:28 +0300

    changed the clamp to screen size

--- MauiApp_Launcher/CursorControls.cs
+++ MauiApp_Launcher/CursorControls.cs
@@ -1,5 +1,5 @@
 ﻿using System.Numerics;
-using System.Threading.Tasks;
+
 
 namespace MauiApp_Launcher
 {
@@ -16,25 +16,45 @@ namespace MauiApp_Launcher
 
 
 
-    public class CursorController : ICursorController
+    public class CursorController : ICursorController, IDisposable
     {
-        private readonly float _imageWidth = 300f;
-        private readonly float _imageHeight = 200f;
+        private  float _screenWidth;
+        private  float _screenHeight;
         private readonly Vector2 _pointerSize;
+        private bool _disposed;
 
         public CursorController(Vector2 pointerSize)
         {
             _pointerSize = pointerSize;
+            UpdateScreenDimensions(DeviceDisplay.MainDisplayInfo);
+            DeviceDisplay.MainDisplayInfoChanged += OnMainDisplayInfoChanged;
         }
 
         public Vector2 CursorTargetPosition { get; private set; } = new(0f, 0f);
 
         public Task SetCursorTargetPositionAsync(Vector2 position)
         {
-            float clampedX = Math.Clamp(position.X, 0f, _imageWidth - _pointerSize.X);
-            float clampedY = Math.Clamp(position.Y, 0f, _imageHeight - _pointerSize.Y);
+            float clampedX = Math.Clamp(position.X, 0f, _screenWidth - _pointerSize.X);
+            float clampedY = Math.Clamp(position.Y, 0f, _screenHeight - _pointerSize.Y);
             CursorTargetPosition = new Vector2(clampedX, clampedY);
             return Task.CompletedTask;
+        }
+
+        private void OnMainDisplayInfoChanged(object? sender, DisplayInfoChangedEventArgs e)
+        {
+            UpdateScreenDimensions(e.DisplayInfo);
+        }
+
+        private void UpdateScreenDimensions(DisplayInfo info)
+        {
+            _screenWidth = (float)(info.Width /*/ info.Density*/);          //DeviceDisplay.MainDisplayInfo gives the screen size in pixels, so dividing by Density converts it to device-independent units (DIPs),
+            _screenHeight = (float)(info.Height /*/ info.Density*/);
+        }
+        public void Dispose()
+        {
+            if (_disposed) return;
+            DeviceDisplay.MainDisplayInfoChanged -= OnMainDisplayInfoChanged;
+            _disposed = true;
         }
     }
     public class XamlCursorUIUpdater : ICursorUIUpdater
