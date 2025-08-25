18ca686463cdf8f8dd99d92aa776970767455a47
dSug1 <98550633+dSug1@users.noreply.github.com>
2025-08-25 19:12:21 +0300

    changed the directories

--- PythonApp_Main.py
+++ PythonApp_Main.py
@@ -3,19 +3,19 @@ import sys
 import os
 import time
 
-from Resources import Launcher
-
+from PythonApp.Resources import Launcher_for_Server_and_Client
+from PythonApp.Resources import Client
 
 # Resolve paths relative to the folder
 _dir = os.path.dirname(os.path.abspath(__file__))
-launcher_path = os.path.join("Resources", "Launcher.py")
+launcher_path = os.path.join("PythonApp", "Resources", "Launcher_for_Server_and_Client.py")
 
 # Launch Launcher.py
 launcher_command = [
     sys.executable,
     launcher_path
 ]
-print(f"[Main.py] Starting Launcher.py")
+print(f"[Main.py] Starting Launcher_for_Server_and_Client.py")
 subprocess.Popen(launcher_command)
 
 time.sleep(3)
